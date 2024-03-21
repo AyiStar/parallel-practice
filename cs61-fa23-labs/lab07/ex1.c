aa97263ec85be2587d4fb1c5e5747692714e7b49
AyiStar <ayistar@outlook.com>
2024-03-21 14:19:39 +0000

    finish lab7

--- cs61-fa23-labs/lab07/ex1.c
+++ cs61-fa23-labs/lab07/ex1.c
@@ -81,12 +81,40 @@ long long int sum_simd_unrolled(int vals[NUM_ELEMS]) {
     __m128i _127 = _mm_set1_epi32(127);
     long long int result = 0;
     /* DO NOT MODIFY ANYTHING ABOVE THIS LINE (in this function) */
-
+    int st_arr[4] = {};
     for(unsigned int w = 0; w < OUTER_ITERATIONS; w++) {
         /* YOUR CODE GOES HERE */
         /* Copy your sum_simd() implementation here, and unroll it */
+        for (unsigned int i = 0; i < NUM_ELEMS / 16 * 16; i += 16) {
+            __m128i sum_v = _mm_setzero_si128();
+            __m128i xv;
+            __m128i mask_v = _mm_cmpgt_epi32(xv, _127);
 
+            xv = _mm_loadu_si128((__m128i*)(vals + i));
+            xv = _mm_and_si128(xv, mask_v);
+            sum_v = _mm_add_epi32(sum_v, xv);
+
+            xv = _mm_loadu_si128((__m128i*)(vals + i + 4));
+            xv = _mm_and_si128(xv, mask_v);
+            sum_v = _mm_add_epi32(sum_v, xv);
+
+            xv = _mm_loadu_si128((__m128i*)(vals + i + 8));
+            xv = _mm_and_si128(xv, mask_v);
+            sum_v = _mm_add_epi32(sum_v, xv);
+
+            xv = _mm_loadu_si128((__m128i*)(vals + i + 12));
+            xv = _mm_and_si128(xv, mask_v);
+            sum_v = _mm_add_epi32(sum_v, xv);
+
+            _mm_storeu_si128((__m128i *) st_arr, sum_v);
+            result += (st_arr[0] + st_arr[1] + st_arr[2] + st_arr[3]);
+        }
         /* Hint: you'll need 1 or maybe 2 tail cases here. */
+        for(unsigned int i = NUM_ELEMS / 16 * 16; i < NUM_ELEMS; i++) {
+            if (vals[i] >= 128) {
+                result += vals[i];
+            }
+        }
     }
 
     /* DO NOT MODIFY ANYTHING BELOW THIS LINE (in this function) */
